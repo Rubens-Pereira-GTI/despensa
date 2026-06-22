7d1dab7972b5bd47f8ed1ec744a481b1ed0feea9
Rubens .P <rubens.costa.tkd@gmail.com>
2026-06-22 14:09:00 -0300

    Initial commit

---
+++ src/test/java/io/github/com/Rubens_Pereira_GTI/despensa/DespensaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.com.Rubens_Pereira_GTI.despensa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespensaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
